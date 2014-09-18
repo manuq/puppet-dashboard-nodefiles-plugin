932aa3bc46c3f7b9567667c54c849b7130a64794
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-09-18 16:00:00 -0300

    Fix view

--- lib/app/controllers/nodefiles_controller.rb
+++ lib/app/controllers/nodefiles_controller.rb
@@ -3,7 +3,8 @@ class NodefilesController < InheritedResources::Base
   self.append_view_path("vendor/plugins/nodefiles/lib/app/views")
 
   def nodefiles
-    @node_files = NodeFile.all
+    @node = Node.find(params[:node_id])
+    @node_files = NodeFile.all(:conditions => ["node_id = ?", params[:node_id]])
   end
 
 end
