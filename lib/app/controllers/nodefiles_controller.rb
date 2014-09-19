f17221fabd7404824b9b4d5e66320a98fb433e73
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-09-19 13:37:23 -0300

    Fix views path for production

--- lib/app/controllers/nodefiles_controller.rb
+++ lib/app/controllers/nodefiles_controller.rb
@@ -1,6 +1,6 @@
 class NodefilesController < InheritedResources::Base
 
-  self.append_view_path("vendor/plugins/nodefiles/lib/app/views")
+  self.append_view_path(File.join(RAILS_ROOT, "vendor/plugins/nodefiles/lib/app/views"))
 
   def nodefiles
     @node = Node.find(params[:node_id])
